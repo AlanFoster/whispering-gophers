9d46b21beab9f7e43ee3d535baabb6cd1186c7d0
Alan Foster <alan@alanfoster.me>
2016-12-24 03:53:43 +0000

    Part 4 - combine listener and dialer

--- main.go
+++ main.go
@@ -7,10 +7,12 @@ import (
 	"encoding/json"
 	"flag"
 	"net"
+	"fmt"
 )
 
 var (
 	sendAddress = flag.String("sendAddress", "localhost:3001", "host:port the address to send to")
+	listenAddress = flag.String("listenAddress", "localhost:3002", "host:port the address to listen to")
 )
 
 type Message struct {
@@ -20,6 +22,41 @@ type Message struct {
 func main() {
 	flag.Parse()
 
+	go connect()
+	listen()
+}
+
+func listen() {
+	listener, err := net.Listen("tcp", *listenAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		connection, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		go serve(connection)
+	}
+}
+
+func serve(connection net.Conn) {
+	defer connection.Close()
+	decoder := json.NewDecoder(connection)
+	for {
+		var message Message
+		err := decoder.Decode(&message)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		fmt.Printf("%#v\n", message)
+	}
+}
+
+
+func connect() {
 	connection, err := net.Dial("tcp", *sendAddress)
 	if err != nil {
 		log.Fatal(err)
